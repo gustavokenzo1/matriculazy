39183bfdf9986f8df9b3da779ac26da4f330e58e
gustavokenzo1 <gustavokenzo314@gmail.com>
2022-09-26 17:52:12 -0300

    add home page animations

--- web/src/components/ExampleItem.tsx
+++ web/src/components/ExampleItem.tsx
@@ -1,4 +1,5 @@
 import { motion } from "framer-motion";
+import { Bed } from "phosphor-react";
 
 interface ExampleItemProps {
   text: string;
@@ -13,7 +14,7 @@ export const ExampleItem = ({ text, position }: ExampleItemProps) => {
       dragConstraints={{ top: 0, left: 0, right: 0, bottom: 0 }}
       className="w-[50px] h-[50px] bg-white rounded flex items-center justify-center font-medium shadow-xl cursor-pointer text-black"
     >
-      {text}
+      {text ? text : <Bed size={24} weight="duotone" color="red" />}
     </motion.div>
   );
 };
--- web/src/page/Home.tsx
+++ web/src/page/Home.tsx
@@ -7,23 +7,41 @@ export const Home = () => {
       <section className="flex min-h-screen w-screen">
         <div className="w-1/2 flex flex-col items-center justify-center">
           <div className="flex flex-col gap-6 ml-20">
-            <h1 className="text-8xl font-bold">MatricuLazy</h1>
-            <p className="text-3xl text-primary-500 font-medium">
+            <motion.h1
+              initial={{ opacity: 0, y: 100 }}
+              animate={{ opacity: 1, y: 0 }}
+              transition={{ duration: 1 }}
+              className="text-8xl font-bold"
+            >
+              MatricuLazy
+            </motion.h1>
+            <motion.p
+              initial={{ opacity: 0 }}
+              animate={{ opacity: 1 }}
+              transition={{ duration: 1, delay: 1 }}
+              className="text-3xl text-primary-500 font-medium"
+            >
               Um gerador de grade horária para pessoas preguiçosas.
-            </p>
+            </motion.p>
           </div>
         </div>
-        <div className="w-1/2 flex items-center justify-center">
+        <motion.div
+          className="w-1/2 flex items-center justify-center"
+          initial={{ opacity: 0 }}
+          animate={{ opacity: 1 }}
+          transition={{ duration: 1, delay: 1.5 }}
+        >
           <motion.div
-            className="bg-brand-500 rounded-xl shadow-lg p-8 grid grid-cols-3 gap-10 transition overflow-hidden"
+            className="bg-brand-500 rounded-xl shadow-lg p-8 grid grid-cols-3 gap-10 overflow-hidden"
             animate={{
-              height: [100, 200, 300, 300, 200, 100],
+              height: [110, 200, 300, 300, 200, 110],
+              opacity: 1,
             }}
             transition={{
               times: [0, 0.1, 0.2, 0.8, 0.9, 1],
               duration: 6,
               loop: Infinity,
-              delay: 0.5,
+              delay: 2,
               repeatDelay: 2,
             }}
           >
@@ -39,7 +57,7 @@ export const Home = () => {
             <ExampleItem text="OO" position={2} />
             <ExampleItem text="" position={2} />
           </motion.div>
-        </div>
+        </motion.div>
       </section>
     </div>
   );
