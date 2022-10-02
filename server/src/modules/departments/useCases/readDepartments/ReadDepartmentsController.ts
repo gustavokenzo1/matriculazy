9d4181be73b2222c5f97d3dbf3227feb50e91b90
gustavokenzo1 <gustavokenzo314@gmail.com>
2022-10-02 16:32:19 -0300

    add docker

--- server/src/modules/departments/useCases/readDepartments/ReadDepartmentsController.ts
+++ server/src/modules/departments/useCases/readDepartments/ReadDepartmentsController.ts
@@ -3,12 +3,12 @@ import { ReadDepartmentsUseCase } from "./ReadDepartmentsUseCase";
 
 export class ReadDepartmentsController {
   async handle(req: Request, res: Response) {
-    const { university } = req.params
+    const { university } = req.query
 
     const readDepartmentsUseCase = new ReadDepartmentsUseCase()
 
     try {
-      const departments = await readDepartmentsUseCase.execute(university)
+      const departments = await readDepartmentsUseCase.execute(university as string)
 
       return res.status(200).json(departments)
     } catch (error) {
